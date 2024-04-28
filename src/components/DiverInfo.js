02240c2d0df9970478e20de0bff2e6735781af05
Nate Paradiso <ipfcolo05@gmail.com>
2024-04-28 13:29:54 -0700

    sending again

--- src/components/DiverInfo.js
+++ src/components/DiverInfo.js
@@ -5,6 +5,8 @@ import LiabilityRelease from "./LiabilityRelease";
 import axios from "axios";
 
 const DiverInfo = ({ selectedDate, setShowDiverInfo, showDiverInfo }) => {
+  const serverUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
   console.log("from calendar", selectedDate);
   console.log(showDiverInfo);
   const [formData, setFormData] = useState({
@@ -452,17 +454,34 @@ const DiverInfo = ({ selectedDate, setShowDiverInfo, showDiverInfo }) => {
     try {
       await sendFormData(formData);
       // Handle success, e.g., show a success message to the user
+      console.log("Form data sent successfully");
     } catch (error) {
       // Handle error, e.g., show an error message to the user
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error("Request failed with status:", error.response.status);
+        console.error("Error message:", error.response.data.error);
+        // Show an error message to the user
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("No response received from server:", error.request);
+        // Show an error message to the user
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error setting up the request:", error.message);
+        // Show an error message to the user
+      }
     }
   };
+
   return (
     <section className="max-w-[1200px] w-full">
       {" "}
       <form
         className="gform "
-        method="POST"
         onSubmit={handleSubmit}
+        method="POST"
         data-email="example@gmail.com"
         action="https://script.google.com/macros/s/AKfycbxn1unr4NE8TQ3_P9sD-rf_jNtqZEjxONZHV4qO_3ILU6iq5r88oFK5JZultmoeIgzUng/exec"
       >
