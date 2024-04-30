7816d7674e2438cd1b2ac3699f236b5806e0c311
Nate Paradiso <ipfcolo05@gmail.com>
2024-04-30 09:19:44 -0700

    privacy policy

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="max-w-[800px] mx-auto p-4 flex flex-col justify-start">
+      <h1 className="text-xl">Privacy Policy</h1>
+      <p>
+        At Devilfish Diving LLC, we take your privacy seriously. This Privacy Policy outlines the
+        types of personal information we collect, how we use it, and your rights regarding this
+        information.
+      </p>
+      <br />
+      <h2 className="text-lg">Information We Collect</h2>
+      <p>
+        We collect information that you provide directly to us, such as when you fill out forms on
+        our website or contact us via email. This may include your name, email address, and any
+        other information you choose to provide.
+      </p>
+      <br />
+      <h2 className="text-lg">How We Use Your Information</h2>
+      <p>
+        We use the information we collect to communicate with you, provide services or information
+        you request, improve our website and services, and comply with legal obligations.
+      </p>
+      <br />
+      <h2 className="text-lg">Information Sharing</h2>
+      <p>
+        We do not sell, trade, or otherwise transfer your personal information to third parties
+        without your consent, except as required by law or as necessary to provide services to you.
+      </p>
+      <br />
+      <h2 className="text-lg">Security</h2>
+      <p>
+        We take reasonable measures to protect the security of your personal information. However,
+        no method of transmission over the internet or electronic storage is completely secure, so
+        we cannot guarantee absolute security.
+      </p>
+      <br />
+      <h2 className="text-lg">Changes to This Privacy Policy</h2>
+      <p>
+        We may update this Privacy Policy from time to time. Any changes will be posted on this
+        page, and the date of the last update will be indicated at the top of the page.
+      </p>
+      <p>
+        By using our website, you consent to the collection and use of your information as outlined
+        in this Privacy Policy.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
