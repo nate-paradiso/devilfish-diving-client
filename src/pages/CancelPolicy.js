c5e35f418ecb92941b234f24c9c2fbf34e22647f
Nate Paradiso <ipfcolo05@gmail.com>
2024-06-10 08:10:58 -0700

    margins

--- src/pages/CancelPolicy.js
+++ src/pages/CancelPolicy.js
@@ -2,48 +2,50 @@ import React from "react";
 
 const CancelPolicy = () => {
   return (
-    <div className="max-w-[800px] p-4 flex flex-col m-auto bg-white bg-opacity-60 shadow-md rounded-md mb-4">
-      <h1 className="text-xl">Cancellation Policy</h1>
-      <br />
-      <p>
-        At Devilfish Diving LLC, we understand that plans can change. Our cancellation policy is
-        designed to provide flexibility and fairness to all our customers.
-      </p>
-      <br />
-      <h2 className="text-lg">Cancellation and Refund Policy</h2>
-      <p>
-        If you need to cancel your booking, please notify us as soon as possible. Our cancellation
-        policy is as follows:
-      </p>
-      <ul className="list-disc list-inside pl-4">
-        <li>
-          Cancellations made at least 2 days (48 hours) before the scheduled service will receive a
-          100% refund.
-        </li>
-        <li>
-          Cancellations made within 2 days (48 hours) of the scheduled service will not be eligible
-          for a refund.
-        </li>
-        <li>
-          In the event of unforeseen circumstances by the charter or weather-related, a 100% refund
-          will be granted.
-        </li>
-      </ul>
-      <br />
-      <h2 className="text-lg">How to Cancel</h2>
-      <p>
-        To cancel your booking, please contact us via email or phone. Be sure to provide your
-        booking details so we can process your cancellation promptly.
-      </p>
-      <br />
-      <h2 className="text-lg">Contact Us</h2>
-      <p>
-        If you have any questions about our cancellation policy, please do not hesitate to contact
-        us.
-      </p>
-      <p>
-        By booking with Devilfish Diving LLC, you agree to the terms of this cancellation policy.
-      </p>
+    <div className="flex justify-center">
+      <div className="max-w-[800px] p-4 flex flex-col  bg-white bg-opacity-60 shadow-md rounded-md m-4">
+        <h1 className="text-xl">Cancellation Policy</h1>
+        <br />
+        <p>
+          At Devilfish Diving LLC, we understand that plans can change. Our cancellation policy is
+          designed to provide flexibility and fairness to all our customers.
+        </p>
+        <br />
+        <h2 className="text-lg">Cancellation and Refund Policy</h2>
+        <p>
+          If you need to cancel your booking, please notify us as soon as possible. Our cancellation
+          policy is as follows:
+        </p>
+        <ul className="list-disc list-inside pl-4">
+          <li>
+            Cancellations made at least 2 days (48 hours) before the scheduled service will receive
+            a 100% refund.
+          </li>
+          <li>
+            Cancellations made within 2 days (48 hours) of the scheduled service will not be
+            eligible for a refund.
+          </li>
+          <li>
+            In the event of unforeseen circumstances by the charter or weather-related, a 100%
+            refund will be granted.
+          </li>
+        </ul>
+        <br />
+        <h2 className="text-lg">How to Cancel</h2>
+        <p>
+          To cancel your booking, please contact us via email or phone. Be sure to provide your
+          booking details so we can process your cancellation promptly.
+        </p>
+        <br />
+        <h2 className="text-lg">Contact Us</h2>
+        <p>
+          If you have any questions about our cancellation policy, please do not hesitate to contact
+          us.
+        </p>
+        <p>
+          By booking with Devilfish Diving LLC, you agree to the terms of this cancellation policy.
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -2,65 +2,68 @@ import React from "react";
 
 const PrivacyPolicy = () => {
   return (
-    <div className="max-w-[800px] p-4 flex flex-col m-auto bg-white bg-opacity-60 shadow-md rounded-md mb-4">
-      <h1 className="text-xl">Privacy Policy</h1>
-      <p>Last updated: May 9, 2024</p>
-      <p>
-        At Devilfish Diving LLC, we take your privacy seriously. This Privacy Policy outlines the
-        types of personal information we collect, how we use it, and your rights regarding this
-        information.
-      </p>
-      <br />
-      <h2 className="text-lg">Information We Collect</h2>
-      <p>
-        We collect information that you provide directly to us, such as when you fill out forms on
-        our website or contact us via email. This may include your name, email address, and any
-        other information you choose to provide.
-      </p>
-      <br />
-      <h2 className="text-lg">How We Use Your Information</h2>
-      <p>
-        We use the information we collect to communicate with you, provide services or information
-        you request, improve our website and services, and comply with legal obligations.
-      </p>
-      <br />
-      <h2 className="text-lg">Information Sharing</h2>
-      <p>
-        We do not sell, trade, or otherwise transfer your personal information to third parties
-        without your consent, except as required by law or as necessary to provide services to you.
-      </p>
-      <br />
-      <h2 className="text-lg">Security</h2>
-      <p>
-        We take reasonable measures to protect the security of your personal information. However,
-        no method of transmission over the internet or electronic storage is completely secure, so
-        we cannot guarantee absolute security.
-      </p>
-      <br />
-      <h2 className="text-lg">Data Retention</h2>
-      <p>
-        We retain your personal information only for as long as necessary for the purposes outlined
-        in this Privacy Policy. The criteria used to determine the retention periods include the
-        nature of the data, the purposes for which it is processed, and any legal or regulatory
-        requirements.
-      </p>
-      <br />
-      <h2 className="text-lg">Data Subject Rights</h2>
-      <p>
-        You have the right to access, rectify, or delete your personal information. If you would
-        like to exercise any of these rights, please contact us using the information provided at
-        the end of this Privacy Policy.
-      </p>
-      <br />
-      <h2 className="text-lg">Changes to This Privacy Policy</h2>
-      <p>
-        We may update this Privacy Policy from time to time. Any changes will be posted on this
-        page, and the date of the last update will be indicated at the top of the page.
-      </p>
-      <p>
-        By using our website, you consent to the collection and use of your information as outlined
-        in this Privacy Policy.
-      </p>
+    <div className="flex justify-center">
+      <div className="max-w-[800px] p-4 flex flex-col bg-white bg-opacity-60 shadow-md rounded-md m-4">
+        <h1 className="text-xl">Privacy Policy</h1>
+        <p>Last updated: May 9, 2024</p>
+        <p>
+          At Devilfish Diving LLC, we take your privacy seriously. This Privacy Policy outlines the
+          types of personal information we collect, how we use it, and your rights regarding this
+          information.
+        </p>
+        <br />
+        <h2 className="text-lg">Information We Collect</h2>
+        <p>
+          We collect information that you provide directly to us, such as when you fill out forms on
+          our website or contact us via email. This may include your name, email address, and any
+          other information you choose to provide.
+        </p>
+        <br />
+        <h2 className="text-lg">How We Use Your Information</h2>
+        <p>
+          We use the information we collect to communicate with you, provide services or information
+          you request, improve our website and services, and comply with legal obligations.
+        </p>
+        <br />
+        <h2 className="text-lg">Information Sharing</h2>
+        <p>
+          We do not sell, trade, or otherwise transfer your personal information to third parties
+          without your consent, except as required by law or as necessary to provide services to
+          you.
+        </p>
+        <br />
+        <h2 className="text-lg">Security</h2>
+        <p>
+          We take reasonable measures to protect the security of your personal information. However,
+          no method of transmission over the internet or electronic storage is completely secure, so
+          we cannot guarantee absolute security.
+        </p>
+        <br />
+        <h2 className="text-lg">Data Retention</h2>
+        <p>
+          We retain your personal information only for as long as necessary for the purposes
+          outlined in this Privacy Policy. The criteria used to determine the retention periods
+          include the nature of the data, the purposes for which it is processed, and any legal or
+          regulatory requirements.
+        </p>
+        <br />
+        <h2 className="text-lg">Data Subject Rights</h2>
+        <p>
+          You have the right to access, rectify, or delete your personal information. If you would
+          like to exercise any of these rights, please contact us using the information provided at
+          the end of this Privacy Policy.
+        </p>
+        <br />
+        <h2 className="text-lg">Changes to This Privacy Policy</h2>
+        <p>
+          We may update this Privacy Policy from time to time. Any changes will be posted on this
+          page, and the date of the last update will be indicated at the top of the page.
+        </p>
+        <p>
+          By using our website, you consent to the collection and use of your information as
+          outlined in this Privacy Policy.
+        </p>
+      </div>
     </div>
   );
 };
