fefbb8f332293434cde9af164515b190d51b2cf7
Nate Paradiso <ipfcolo05@gmail.com>
2024-12-08 21:16:25 -0800

    added dving date to paypal desc

--- src/components/PayPalCart.js
+++ src/components/PayPalCart.js
@@ -4,48 +4,48 @@ export const cartItems = [
     name: "Dive",
     quantity: 1,
     price: 145, // Price for Dive
-    description: "Dive trip with Devilfish Diving - 1st Diver", // Add description here
+    description: "1st Diver", // Add description here
   },
   {
     id: "1 Dive Seat",
     name: "1 Dive Seat",
     quantity: 1,
     price: 145, // Price for Dive
-    description: "Dive trip with Devilfish Diving - 2nd Diver", // Add description here
+    description: "2nd Diver", // Add description here
   },
   {
     id: "Cruise",
     name: "Cruise",
     quantity: 1,
     price: 210, // Price for Cruise
-    description: "A 2.5hr cruise with Devilfish Diving", // Add description here
+    description: "A 2.5hr cruise", // Add description here
   },
   {
     id: "3rd Diver",
     name: "3rd Diver",
     quantity: 1,
     price: 100, // Price for 3rd Diver
-    description: "Dive trip with Devilfish Diving - 3rd Diver", // Add description here
+    description: "3rd Diver", // Add description here
   },
   {
     id: "Entire Charter",
     name: "Entire Charter",
     quantity: 1,
     price: 290, // Price Entire Charter
-    description: "Dive trip with Devilfish Diving - Entire Charter", // Add description here
+    description: "Entire Charter", // Add description here
   },
   {
     id: "Entire Charter - 3 Divers",
     name: "Entire Charter - 3 Divers",
     quantity: 1,
     price: 390, // Price Entire Charter
-    description: "Dive trip with Devilfish Diving - Entire Charter - 3 Divers", // Add description here
+    description: "Entire Charter - 3 Divers", // Add description here
   },
   {
     id: "Class - 3 Divers",
     name: "Class - 3 Divers",
     quantity: 1,
     price: 495, // Price Entire Charter
-    description: "Dive trip with Devilfish Diving - Class - 3 Divers", // Add description here
+    description: "Class - 3 Divers", // Add description here
   },
 ];
