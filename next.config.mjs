2408f95176423df83b13ccedcc833af0bb037ea6
Nate Paradiso <ipfcolo05@gmail.com>
2025-08-24 21:40:49 -0700

    shaded relieft full screen and tracking

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,13 @@
-// next.config.js
-import withFonts from "next-fonts";
-
-export default withFonts({
-  webpack(config, options) {
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/arcgis-proxy/:path*",
+        destination: "http://localhost:8888/:path*",
+      },
+    ];
   },
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  screens: {
-    sm: "640px",
-    // => @media (min-width: 640px) { ... }
+};
 
-    md: "768px",
-    // => @media (min-width: 768px) { ... }
-
-    lg: "1024px",
-    // => @media (min-width: 1024px) { ... }
-
-    xl: "1280px",
-    // => @media (min-width: 1280px) { ... }
-
-    "2xl": "1536px",
-    // => @media (min-width: 1536px) { ... }
-  },
-});
+export default nextConfig;
